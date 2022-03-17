df5eff57bc24015df5f386b2872935bd44370b06
ali khan <alikhan40310@gmail.com>
2022-03-17 13:27:04 +0500

    validate register form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,37 @@
 <template>
-  <div class="container">
-    <h2>hello logifsddfsn</h2>
+  <div class="cusStyle">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-8 col-lg-6">
+          <div class="card">
+            <div class="card-header">
+              <div class="card-title">Login User</div>
+            </div>
+            <div class="card-body">
+              <form class="gap-2 d-flex flex-column">
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Username"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="password"
+                    class="form-control"
+                    placeholder="Password"
+                  />
+                </div>
+                <button class="btn btn-outline-success form-control">
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,8 +40,10 @@ export default {};
 </script>
 
 <style>
-h2 {
-  font-size: 50px;
-  /* color: white; */
-}
+    .cusStyle{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 80vh;
+    }
 </style>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="register">
+  <div class="cusStyle">
     <div class="container">
       <div class="newstyle">
         <div class="row justify-content-center">
@@ -9,34 +9,57 @@
                 <div class="card-title">Register User</div>
               </div>
               <div class="card-body">
-                <form class="gap-2 d-flex flex-column">
+                <form
+                  class="gap-2 d-flex flex-column"
+                  @submit.prevent="registerUser"
+                >
                   <div class="form-group">
                     <input
+                      v-model="username"
                       type="text"
                       class="form-control"
                       placeholder="Username"
+                      required
                     />
+                    <div class="error" v-if="userValidate">
+                      {{ userValidate }}
+                    </div>
                   </div>
                   <div class="form-group">
                     <input
+                      v-model="email"
                       type="email"
                       class="form-control"
                       placeholder="Email"
+                      required
                     />
+                    <div class="error" v-if="emailValidate">
+                      {{ emailValidate }}
+                    </div>
                   </div>
                   <div class="form-group">
                     <input
+                      v-model="password"
                       type="password"
                       class="form-control"
                       placeholder="Password"
+                      required
                     />
+                    <div class="error" v-if="passwordValidate">
+                      {{ passwordValidate }}
+                    </div>
                   </div>
                   <div class="form-group">
                     <input
+                      v-model="confirmPassword"
                       type="password"
                       class="form-control"
                       placeholder="Confirm Password"
+                      required
                     />
+                    <div class="error" v-if="confirmPasswordValidate">
+                      {{ confirmPasswordValidate }}
+                    </div>
                   </div>
                   <button class="btn btn-outline-success form-control">
                     Register
@@ -52,13 +75,63 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Register",
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      userValidate: "",
+      emailValidate: "",
+      passwordValidate: "",
+      confirmPasswordValidate: "",
+    };
+  },
+  methods: {
+    // validate form
+    registerUser() {
+      // username validation
+      if (this.username.length < 3) {
+        this.userValidate = "Username must be at least 3 characters";
+        return;
+      } else {
+        this.userValidate = "";
+      }
+      // email validation regex
+      if (
+        !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          this.email
+        )
+      ) {
+        this.emailValidate = "Email is not valid";
+        return;
+      } else {
+        this.emailValidate = "";
+      }
+      // password validation
+      if (this.password.length < 6) {
+        this.passwordValidate = "Password must be at least 6 characters";
+        return;
+      } else {
+        this.passwordValidate = "";
+      }
+      // confirm password validation
+      if (this.password !== this.confirmPassword) {
+        this.confirmPasswordValidate = "Password does not match";
+        return;
+      } else {
+        this.confirmPasswordValidate = "";
+      }
+    },
+  },
+};
 </script>
 
 <style>
-.newstyle {
-  /* height: 100vh; */
-  /* display: flex; */
-  /* width: 100%; */
+.error {
+  color: red;
+  font-size: 12px;
 }
 </style>
